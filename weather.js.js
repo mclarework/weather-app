b20e22763f902d30051f32543acb029bcb059142
CN18 <codenation@CN18s-MBP.lan>
2020-03-03 15:58:11 +0000

    Finished for now

--- weather.js.js
+++ weather.js.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const apiKey = "fe4fce40a84459165693c45b9787d939";
+const apiKey = "";
 
 const getWeather = async (locationObject) => {
   const {lat,lng,location} = locationObject
