0b34999b12779617c55febc58cb77368f3c48a93
CN18 <codenation@CN18s-MBP.lan>
2020-03-03 15:55:29 +0000

    moving on

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const { getWeather } = require("./weather.js");
 const { getLocation } = require("./location.js");
+const figlet = require("figlet")
 
 const main = async(input) => {
     const locationObject = await getLocation(input)
     await getWeather(locationObject)
 };
 
-main(process.argv[2])
+if (startup == true ) {
+    figlet("My Weather App!", (err,data)=> {
+        if (err) {
+            console.log("Something went wrong!")
+            console.dir(err)
+            return
+        }
+        console.log(data)
+        main(process.argv[2])
+    })
+}
+
